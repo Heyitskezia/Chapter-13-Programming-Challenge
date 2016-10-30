51ea4604596c30b6ba856f623045c2fc43d2434a
Heyitskezia <Nathania98@gmail.com>
2016-10-31 01:01:31 +0700

    Page 802  no. 5.cpp

--- Page 802  no. 5.cpp
+++ Page 802  no. 5.cpp
@@ -38,19 +38,19 @@ class Retail
 
 int main(){
 	Retail item1 = Retail("Jacket\t", 12, 59.95);
-	Retail item2 = Retail("Designer Jeans\t", 40, 34.95);
+	Retail item2 = Retail("Jeans\t", 40, 34.95);
 	Retail item3 = Retail("Shirt\t", 20, 24.95);
 	
 	Retail item [3] = {item1, item2, item3};
 	string numOfItem [3] = {"Item #1\t", "Item #2\t", "Item #3\t"};
 	string row [3] = {"\tDescription", "Units of Hand (Pieces)", "\tPrice ($)"};
 	
-	for (int x = 0 ; x < 3 ; x++){
-		cout << "\t" << row[x] << " \t";
+	for (int i = 0 ; i <= 2 ; i++){
+		cout << "\t" << row[i] << " \t";
 	}
 	
-	for (int y = 0 ; y < 3 ; y++){
-		cout << endl << endl << numOfItem[y] << "\t " << item[y].getDesc() << "\t\t" << item[y].getUnits() << "\t\t\t\t" << item[y].getPrice() << endl;
+	for (int j = 0 ; j <= 2 ; j++){
+		cout << endl << endl << numOfItem[j] << "\t " << item[j].getDesc() << "\t\t" << item[j].getUnits() << "\t\t\t\t" << item[j].getPrice() << endl;
 	}
 	
 	return 0;
